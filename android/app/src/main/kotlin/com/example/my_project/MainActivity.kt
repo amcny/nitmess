aab89cb1df429764197d9ad01c39e971dee95b80
amcny <cny2045@gmail.com>
2024-02-11 20:42:03 +0530

    cleaning

--- android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.nitmess.com
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/custom_code/actions/index.dart
+++
@@ -1,3 +0,0 @@
-export 'lock_orientation.dart' show lockOrientation;
-export 'in_app_update.dart' show inAppUpdate;
-export 'version_actual.dart' show versionActual;
